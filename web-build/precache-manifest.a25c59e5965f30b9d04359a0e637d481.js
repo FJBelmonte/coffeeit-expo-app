4ce5b0d852a6c8ffaa1bce80038ee66cdc9ca131
Fernando Belmonte <fernando.belmonte@live.com>
2019-11-16 23:49:11 -0300

    page

---
+++ web-build/precache-manifest.a25c59e5965f30b9d04359a0e637d481.js
@@ -0,0 +1,143 @@
+self.__precacheManifest = [
+  {
+    "revision": "f1e0acfd70bb19eb7e2a",
+    "url": "/static/js/app.190e828f.chunk.js"
+  },
+  {
+    "revision": "6e94c2c50ed5cc137f4f",
+    "url": "/static/js/runtime~app.73b29d71.js"
+  },
+  {
+    "revision": "1dc3c653093d36b5e5c9",
+    "url": "/static/js/2.9b648506.chunk.js"
+  },
+  {
+    "revision": "6165c9d7a2e729ba57b23dd93add5366",
+    "url": "/static/media/back-icon-mask.6165c9d7.png"
+  },
+  {
+    "revision": "5a293a273bee8d740a045d9922b9a9ae",
+    "url": "/./fonts/MaterialCommunityIcons.ttf"
+  },
+  {
+    "revision": "b2e0fc821c6886fb3940f85a3320003e",
+    "url": "/./fonts/Ionicons.ttf"
+  },
+  {
+    "revision": "3a2ba31570920eeb9b1d217cabe58315",
+    "url": "/./fonts/AntDesign.ttf"
+  },
+  {
+    "revision": "744ce60078c17d86006dd0edabcd59a7",
+    "url": "/./fonts/Entypo.ttf"
+  },
+  {
+    "revision": "6beba7e6834963f7f171d3bdd075c915",
+    "url": "/./fonts/Feather.ttf"
+  },
+  {
+    "revision": "b06871f281fee6b241d60582ae9369b9",
+    "url": "/./fonts/FontAwesome.ttf"
+  },
+  {
+    "revision": "c6aef942e3668158ec29d4adcb2e768f",
+    "url": "/./fonts/FontAwesome5_Brands.ttf"
+  },
+  {
+    "revision": "872545dde71de3842234bf6afe80c4cb",
+    "url": "/./fonts/FontAwesome5_Solid.ttf"
+  },
+  {
+    "revision": "e20945d7c929279ef7a6f1db184a4470",
+    "url": "/./fonts/Foundation.ttf"
+  },
+  {
+    "revision": "a37b0c01c0baf1888ca812cc0508f6e2",
+    "url": "/./fonts/MaterialIcons.ttf"
+  },
+  {
+    "revision": "d2285965fe34b05465047401b8595dd0",
+    "url": "/./fonts/SimpleLineIcons.ttf"
+  },
+  {
+    "revision": "e58391710fd9cee976deca36423611e7",
+    "url": "/favicon.ico"
+  },
+  {
+    "revision": "5e695e96a003a79f7f97060bf49409a9",
+    "url": "/expo-service-worker.js"
+  },
+  {
+    "revision": "d0c694b562b2208635f250762cd7fc79",
+    "url": "/serve.json"
+  },
+  {
+    "revision": "9489ed250552537e3f7ddf137523bac0",
+    "url": "/manifest.json"
+  },
+  {
+    "url": "/apple/icons/icon_180x180.089fd7a673663bce35ad27bfb7a31b9f.png"
+  },
+  {
+    "url": "/apple/icons/icon_192x192.6e15afa75658d2c45e0a689f7bfa5fe9.png"
+  },
+  {
+    "url": "/apple/icons/icon_512x512.8b7756631a517e03d0809f12b5d1b0ce.png"
+  },
+  {
+    "url": "/apple/splash/icon_1125x2436.662e0a1ea16dceaf82c07b1427e2b955.png"
+  },
+  {
+    "url": "/apple/splash/icon_1136x640.08197976da96d36e76f919de080b1ab6.png"
+  },
+  {
+    "url": "/apple/splash/icon_1242x2208.99ec36ff008d09edf7e0238c1a98502d.png"
+  },
+  {
+    "url": "/apple/splash/icon_1242x2688.275992a939c034c434d2163e2e44c030.png"
+  },
+  {
+    "url": "/apple/splash/icon_1334x750.0782557f0592c221d1095c3da2152500.png"
+  },
+  {
+    "url": "/apple/splash/icon_1792x828.f243c583e6bf14fb49e0720c4cff666d.png"
+  },
+  {
+    "url": "/apple/splash/icon_2208x1242.f1554b8c937b55b2627606c6e8310a69.png"
+  },
+  {
+    "url": "/apple/splash/icon_2436x1125.6ba88ba2c50e51739c3e7b8de55314be.png"
+  },
+  {
+    "url": "/apple/splash/icon_2688x1242.712517369e2ff840caf4539391c04d53.png"
+  },
+  {
+    "url": "/apple/splash/icon_640x1136.5ee893845aad1a5aa227897a1f92dc60.png"
+  },
+  {
+    "url": "/apple/splash/icon_750x1334.584542b619b3813a875a138dedb84aa8.png"
+  },
+  {
+    "url": "/apple/splash/icon_828x1792.cde3b487d0d8b147bd413b564a11ff91.png"
+  },
+  {
+    "revision": "fd34b8d8c8a2fb1d029a269347fdee1c",
+    "url": "/index.html"
+  },
+  {
+    "revision": "70db53451110faaedda75785154714dd",
+    "url": "/static/js/runtime~app.73b29d71.js.gz"
+  },
+  {
+    "revision": "bd56db0844d50841ee6e0b69eb5e794d",
+    "url": "/expo-service-worker.js.gz"
+  },
+  {
+    "revision": "bdcda608dbd729b053f82303120c0df5",
+    "url": "/static/js/app.190e828f.chunk.js.gz"
+  },
+  {
+    "revision": "9cde1def97bb9b11e06869dbe79b19d3",
+    "url": "/static/js/2.9b648506.chunk.js.gz"
+  }
+];
